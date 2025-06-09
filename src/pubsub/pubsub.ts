72d70302b1bcf5e7318ad8f700e5449948ca7267
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-06-09 21:37:10 +0530

    reverting back to functions

--- src/pubsub/pubsub.ts
+++ src/pubsub/pubsub.ts
@@ -7,39 +7,39 @@ interface Subscriber<T = any> {
   timestamp?: number;
 }
 
-class PubSub {
-    private events: Record<string, Subscriber[]> = {};
+
+const pubsub = {
+  events: {} as Record<string, Subscriber[]>,
   
-    subscribe<T>(event: string, fn: EventCallback<T>, componentName: string = 'Anonymous') {
-      if (!this.events[event]) {
-        this.events[event] = [];
-      }
-      this.events[event].push({
-        fn,
-        componentName,
-        timestamp: Date.now(),
+  subscribe<T>(event: string, fn: EventCallback<T>, componentName: string = 'Anonymous') {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push({
+      fn,
+      componentName,
+      timestamp: Date.now(),
+    });
+  },
+
+  unsubscribe<T>(event: string, fn: EventCallback<T>) {
+    if (this.events[event]) {
+      this.events[event] = this.events[event].filter(sub => sub.fn !== fn);
+    }
+  },
+
+  publish<T>(event: string, data: T) {
+    if (this.events[event]) {
+      this.events[event].forEach(({ fn, componentName }) => {
+        try {
+          fn(data);
+        } catch (err) {
+          console.error(`[${componentName}] Error in "${event}" subscriber:`, err);
+        }
       });
     }
-  
-    unsubscribe<T>(event: string, fn: EventCallback<T>) {
-      if (this.events[event]) {
-        this.events[event] = this.events[event].filter(sub => sub.fn !== fn);
-      }
-    }
-  
-    publish<T>(event: string, data: T) {
-      if (this.events[event]) {
-        this.events[event].forEach(({ fn, componentName }) => {
-          try {
-            fn(data);
-          } catch (err) {
-            console.error(`[${componentName}] Error in "${event}" subscriber:`, err);
-          }
-        });
-      }
-    }
   }
+};
 
-const pubsub = new PubSub();
 
 export default pubsub;
