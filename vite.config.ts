ec4132fc08b118ecb9786f2005786240908796f4
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-06-14 00:16:45 +0530

    Build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      entryRoot: 'src',
+      insertTypesEntry: true,
+    })
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'overwatch',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `overwatch.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+        }
+      }
+    },
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+});
